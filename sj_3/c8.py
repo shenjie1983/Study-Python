64dd56e45b32e533dc38c8da1cfd2aa93b1aaff3
shenjie19831 <48176083@qq.com>
2019-04-29 11:08:04 +0800

    更新

--- sj_3/c8.py
+++ sj_3/c8.py
@@ -1,14 +1,22 @@
-from c9 import People
+from c9 import Human
+
 # 建议一个模块建立一个类
 # 继承
-class Student(People):
-    sum1 = 0
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-        self.__score = 0
-        self.__class__.sum1 += 1
+class Student(Human):
+    def __init__(self, school, name, age):
+        self.school = school
+        Human.__init__(self, name, age)
+        # 类调用方法必须要self,实在要调用，传入实力本身
+        # Student.do_homework(student1)
+        # 实列对象则不需要，调用的方法省略self，
+        #
 
     def do_homework(self):
         print('homework')
 
+student1 = Student('人民路小学', "小明", 18)
+# print(student1.sum)
+# print(Student.sum)
+print(student1.name)
+print(student1.age)
+# student1.get_name()
