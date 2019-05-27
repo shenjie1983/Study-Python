2f9242367aed7f33ba014ef5b66e7d278b7bf1a4
shenjie1983 <48176083@qq.com>
2019-05-27 15:50:57 +0800

    联系

---
+++ study_2/hello.py
@@ -0,0 +1,26 @@
+# Flask类的实例是WSGI 应用程序WSGI是Web Server Gateway Interface的缩写。
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    pass
+    
+@app.route('/login')
+def login():
+    pass
+
+@app.route('/projects/<username>')
+def hello_world(username):
+    pass
+
+with app.test_request_context():
+    print url_for('index')
+    print url_for('login')
+    print url_for('login', next='/')
+    print url_for('profile', username='John Doe')
+
+    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
