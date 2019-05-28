30915343f0759d72143162924cc783106d1d8ef0
shenjie1983 <48176083@qq.com>
2019-05-28 10:06:28 +0800

    xiugai

--- study_2/hello.py
+++ study_2/hello.py
@@ -15,11 +15,11 @@ def login():
 def hello_world(username):
     pass
 
-with app.test_request_context():
-    print url_for('index')
-    print url_for('login')
-    print url_for('login', next='/')
-    print url_for('profile', username='John Doe')
+# with app.test_request_context():
+#     print url_for('index')
+#     print url_for('login')
+#     print url_for('login', next='/')
+#     print url_for('profile', username='John Doe')
 
     
 if __name__ == '__main__':
