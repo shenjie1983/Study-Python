8b143ce79174b9b2e6a012caaf8cb5628b959722
shenjie1983 <48176083@qq.com>
2019-09-26 11:52:16 +0800

    tijiao

--- Day01-15/code/Day01/circle.py
+++ Day01-15/code/Day01/circle.py
@@ -8,7 +8,7 @@ import math
 
 radius = float(input('请输入圆的半径：'))
 perimeter = 2 * math.pi * radius
-area = math.pi * radius * radius
+area = math.pi*radius*radius
 print('周长：%.2f 厘米' % perimeter)
 print('面积: %.2f 平方厘米' % area)
 print(math.pi)
---
+++ Day01-15/code/Day01/sb.py
@@ -0,0 +1,12 @@
+'''
+@Descripttion: 
+@version: 0.1
+@Author: 沈洁
+@Date: 2019-09-26 11:42:39
+@LastEditors: 沈洁
+@LastEditTime: 2019-09-26 11:42:39
+'''
+a = 10
+b = 11
+a = a+b
+print(a)
