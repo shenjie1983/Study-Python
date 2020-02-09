37d3c43c64c5aec8d5d8180b3c0c9376b6303358
shenjie19831 <48176083@qq.com>
2020-02-09 11:38:06 +0800

    12

---
+++ crash_course/cs01/test1.py
@@ -0,0 +1,11 @@
+'''
+@Descripttion: 
+@version: 0.1
+@Author: 沈洁
+@Date: 2020-01-09 10:47:34
+@LastEditors  : 沈洁
+@LastEditTime : 2020-01-09 11:04:50
+'''
+print("Hello Python world!")
+message = "Hell Python world!"
+print(message)
